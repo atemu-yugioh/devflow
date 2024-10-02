367ea1f771c8312d478142f4e68f6574c123686b
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-02 16:02:47 +0700

    set up routes

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      Ask Question
+    </main>
+  );
+};
+
+export default page;
