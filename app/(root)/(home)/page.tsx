1ea94d719c6f0a2e2a4faea6f6650693a946efd5
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-02 16:51:56 +0700

    set up clerk provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,20 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <p className="h1-bold text-light-500">this is text</p>
     </main>
   );
