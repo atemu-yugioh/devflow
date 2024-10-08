0529ecd59b5171c3504b780ae059518b2e7f94d6
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-08 13:59:52 +0700

    rename webhook to webhooks

--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
 
-const publicRoutes = createRouteMatcher([
-  "/",
-  "/question/:id",
-  "/tags",
-  "/tags/:id",
-  "/profile/:id",
-  "/community",
-  "/jobs",
-]);
-
-const ignoredRoutes = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
-
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-
-  if (ignoredRoutes(req)) {
-    return; // Bỏ qua middleware cho các ignored routes
-  }
 });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 
 export const config = {
   matcher: [
