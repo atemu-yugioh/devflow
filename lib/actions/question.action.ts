5ae39e372a7e558777b1eecaa9cc70fdfab04c70
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-08 16:28:45 +0700

    create question detail page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { GetQuestionByIdParams } from "./shared.types.d";
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
@@ -15,6 +16,31 @@ export async function getQuestions(params: GetQuestionsParams) {
       .populate({ path: "author", model: User })
       .sort({ createdAt: -1 });
     return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const question = await Question.findById(questionId)
+      .populate({
+        path: "tags",
+        model: Tag,
+        select: "_id name",
+      })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      });
+
+    return question;
   } catch (error) {
     console.log(error);
     throw error;
