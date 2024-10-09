413de2d6bcd528ccfa1e8622fe74b8ed50a182f0
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-09 11:41:17 +0700

    create tag page detail

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,35 +1,28 @@
 import QuestionCard from "@/components/cards/QuestionCard";
-import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import { QuestionFilters } from "@/constants/filter";
-import { getSavedQuestions } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs/server";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import { URLProps } from "@/types";
 import React from "react";
 
-const Page = async () => {
-  const { userId } = auth();
-
-  if (!userId) return null;
-
-  const result = await getSavedQuestions({
-    clerkId: userId,
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
   });
 
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
         <LocalSearchbar
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search for questions"
+          placeholder="Search tag questions"
           otherClasses="flex-1"
-        />
-        <Filter
-          filters={QuestionFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
 
@@ -50,7 +43,7 @@ const Page = async () => {
           ))
         ) : (
           <NoResult
-            title="There’s no question saved to show"
+            title="There’s no tag question saved to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
