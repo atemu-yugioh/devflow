ea54988fc3aeb26a223a66b4bc5a0a6c684385e6
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-10 08:32:22 +0700

    fetch and show top question and popular tag

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import RenderTag from "./RenderTag";
-import { hotQuestions, popularTags } from "@/metadata";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -12,7 +15,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -37,7 +40,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
