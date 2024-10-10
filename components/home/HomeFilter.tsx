13d4e0072f620935092ed8d110e7d11f99afd9f0
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2024-10-10 10:09:08 +0700

    manage filter state

--- components/home/HomeFilter.tsx
+++ components/home/HomeFilter.tsx
@@ -1,9 +1,41 @@
+"use client";
+
 import { HomePageFilters } from "@/constants/filter";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilter = () => {
-  const active = "frequent";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (value: string) => {
+    if (active === value) {
+      setActive("");
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(value);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: value.toString(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
@@ -14,6 +46,7 @@ const HomeFilter = () => {
               ? "bg-primary-100 dark:bg-dark-400 hover:bg-primary-100 dark:hover:bg-dark-400 text-primary-500"
               : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
           }`}
+          onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
